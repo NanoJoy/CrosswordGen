60583b9719096a0e975955b0ab78d85898e7a463
NanoJoy <samuelevlafleur@gmail.com>
2024-07-01 23:18:04 -0400

    cache wordfilter.

--- CrossWeb/Providers/WordFilterProvider.cs
+++ CrossWeb/Providers/WordFilterProvider.cs
@@ -1,5 +1,4 @@
 ﻿using Crossword;
-using System.Net;
 using System.Text;
 
 namespace CrossWeb.Providers
@@ -12,13 +11,17 @@ namespace CrossWeb.Providers
         {
             get
             {
-                var url = "https://peterbroda.me/crosswords/wordlist/lists/peter-broda-wordlist__gridtext__scored__july-25-2023.txt";
-                using var client = new HttpClient();
-                var task = client.GetStringAsync(url);
-                task.Wait();
-                var wordListContent = task.Result;
-                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(wordListContent));
-                _filter ??= new WordFilter(stream);
+                if (_filter == null)
+                {
+                    var url = "https://peterbroda.me/crosswords/wordlist/lists/peter-broda-wordlist__gridtext__scored__july-25-2023.txt";
+                    using var client = new HttpClient();
+                    var task = client.GetStringAsync(url);
+                    task.Wait();
+                    var wordListContent = task.Result;
+                    using var stream = new MemoryStream(Encoding.UTF8.GetBytes(wordListContent));
+                    _filter = new WordFilter(stream);
+                }
+                
                 return _filter;
             }
         }
